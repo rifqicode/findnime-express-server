1ca7499b84956593431a63437700c85ce1bbad6a
rifqicode <muhammadrifqi.tb@gmail.com>
2020-08-28 11:08:02 +0700

    update

--- src/scrapping.js
+++ src/scrapping.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 class Scrapping {
+
   async getAnimeLink() {
     try {
       const url = 'https://moenime.web.id/tag/ongoing/';
@@ -34,15 +35,11 @@ class Scrapping {
   async getEpisode() {
     try {
       const animeLink = await this.getAnimeLink();
-
-      var result = []
-
+      let result = []
       for (const item of animeLink.slice(0, 1)) {
-
         result.push(await this.getEpisodeList(item));
       }
 
-      console.log(result);
       return result;
     } catch (e) {
       return []
@@ -51,37 +48,42 @@ class Scrapping {
 
   async getEpisodeList(item) {
     try {
-      var result = {};
-      var { data } = await axios.get(item.link),
-          $ = cheerio.load(data);
+      const downloadQuality = '480p';
+      const { data } = await axios.get(item.link),
+            $ = cheerio.load(data);
 
-      console.log('running');
       var tableContent = $('div.entry-content').find('table.table.table-hover').slice(2),
           totalEpisode = tableContent.length / 2,
           result = {
             title: item.title,
             totalEpisode: totalEpisode,
             dataEpisode: []
-          };
+          },
+          keyEpisode = 1;
 
       for (var i = 0; i < totalEpisode; i++) {
-        var title = $(tableContent[i]).find('tbody > tr > td > center').text(),
-            listEpisode = $(tableContent[i+1]).find('tbody tr'),
-            dataEpisode = [];
-
-        var current = 0,
+        let title = `Episode ${i + 1}`,
+            listEpisode = $(tableContent[keyEpisode]).find('tbody tr'),
+            dataEpisode = [],
+            current = 0,
             next = 1;
 
         for (var j = 0; j < listEpisode.length / 2 ; j++) {
-           var episodeTitle = $(listEpisode[current]).text(),
-               episodeListLink = [],
-               downloadEpisodeList = $(listEpisode[next]).find('td > center > a').map((i, element) => {
-                 var element = $(element);
-                 return episodeListLink.push({
-                   title : element.text(),
-                   link  : element.attr('href')
-                 })
-               });
+           let episodeTitle = $(listEpisode[current]).text(),
+               episodeListLink = '';
+
+            if (!episodeTitle.includes(downloadQuality)) {
+              current += 2;
+              next += 2;
+              continue;   
+            };
+
+            $(listEpisode[next]).find('td > center > a').map((key, element) => {
+              var element = $(element);
+              
+              if( element.text().toUpperCase() !== 'ZIPPYSHARE') return false;
+              episodeListLink = element.attr('href');
+            });
 
            dataEpisode.push({
              linkTitle: episodeTitle,
@@ -93,14 +95,13 @@ class Scrapping {
         }
 
         result.dataEpisode.push({
-          title: `Episode ${i+1}`,
+          title: title,
           data : dataEpisode
         });
 
-        console.log('get episode');
+        keyEpisode += 2;
       }
 
-      console.log('end');
       return result;
     } catch (e) {
       return {}
